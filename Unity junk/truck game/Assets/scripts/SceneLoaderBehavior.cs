e92b5e4b50d3a2523e6cd434ab186e8c59c8738a
Unknown <10841219@my.uvu.edu>
2021-04-27 16:42:42 -0600

    added the ability to reload the scene

---
+++ Unity junk/truck game/Assets/scripts/SceneLoaderBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderBehavior : MonoBehaviour
+{
+    [SerializeField] private GameObject button;
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void Awake()
+    {
+        button.SetActive(false);
+    }
+}
